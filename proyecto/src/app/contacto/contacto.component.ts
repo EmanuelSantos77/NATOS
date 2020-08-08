c194704a97329902ef8f1778cdac96a5830e6fcc
chatblack99 <chatblack1999@gmail.com>
2020-08-07 23:43:33 -0500

    Final contact comit
    Add new functions to contact component.

--- proyecto/src/app/contacto/contacto.component.ts
+++ proyecto/src/app/contacto/contacto.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef} from '@angular/core';
+
+
 
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.component.html',
-  styleUrls: ['./contacto.component.css']
-})
-export class ContactoComponent implements OnInit {
+  styleUrls: ['./contacto.component.css',
 
-  constructor() { }
+]})
+
+
+export class ContactoComponent implements OnInit {
+  asunto: string;
+  nombre: string;
+  correo: string;
+  txt: string;
+  url: string;
+
+  getAsunto(val){
+    this.asunto = val;
+  }
+  getNombre(val){
+    this.nombre = val;
+  }
+  getCorreo(val){
+    this.correo = val;
+  }
+  // getNumero(val){
+  //   this.numero = val;
+  // }
+  getTxt(val){
+    this.txt = val;
+  }
+
+  generateUrl(){
+      this.url = "https://api.whatsapp.com/send?phone=+5216644198479"+"&text="+this.txt;
+   }
+
+
+
+
+
+  constructor() {
+
+    
+   }
 
   ngOnInit(): void {
+
   }
+  
 
 }
