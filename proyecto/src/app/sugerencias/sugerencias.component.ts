b37433da7da76733835a6bc7a8f5bc080a254154
chatblack99 <chatblack1999@gmail.com>
2020-08-09 15:21:27 -0500

    Feedback component updated.
    Feedback form addded.

--- proyecto/src/app/sugerencias/sugerencias.component.ts
+++ proyecto/src/app/sugerencias/sugerencias.component.ts
@@ -6,6 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sugerencias.component.css']
 })
 export class SugerenciasComponent implements OnInit {
+  asunto: string;
+  nombre: string;
+  correo: string;
+  txt: string;
+  url: string;
+
+  getAsunto(val){
+    this.asunto = val;
+  }
+  getNombre(val){
+    this.nombre = val;
+  }
+  getCorreo(val){
+    this.correo = val;
+  }
+  // getNumero(val){
+  //   this.numero = val;
+  // }
+  getTxt(val){
+    this.txt = val;
+  }
+
+  generateUrl(){
+      this.url = "https://api.whatsapp.com/send?phone=+5216644198479"+"&text="+this.txt;
+   }
 
   constructor() { }
 
