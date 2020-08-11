3bfb9138bf529c079c3049ab61be582ed841edd3
chatblack99 <chatblack1999@gmail.com>
2020-08-11 01:13:53 -0500

    embed google sheet
    implemented with log aut.

--- proyecto/src/app/sugerencias/sugerencias.component.ts
+++ proyecto/src/app/sugerencias/sugerencias.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../servicios/login.service';
 
 @Component({
   selector: 'app-sugerencias',
@@ -11,6 +12,7 @@ export class SugerenciasComponent implements OnInit {
   correo: string;
   txt: string;
   url: string;
+  entrar=false;
 
   getAsunto(val){
     this.asunto = val;
@@ -32,9 +34,17 @@ export class SugerenciasComponent implements OnInit {
       this.url = "https://api.whatsapp.com/send?phone=+5216644198479"+"&text="+this.txt;
    }
 
-  constructor() { }
+  constructor(private loginservicio:LoginService) { }
 
   ngOnInit(): void {
+    this.loginservicio.change.subscribe(isOpen =>{
+      this.entrar = isOpen;
+    })
+    this.llenarentrar();
+  }
+  llenarentrar(){
+    this.entrar=this.loginservicio.eslogueado();
+
   }
 
 }
